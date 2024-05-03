bfe4642b5d739664f98789d4f288f96d38258433
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2024-05-03 00:34:50 -0300

    cria novo endpoint que retorna um html

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,20 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from .schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Olá mundo!"}
+
+
+@app.get("/exercise-1", status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def exercise_1():
+    return """
+    <h1>Olá mundo!</h1>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,12 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Olá mundo!"}
+
+
+def test_exercise_1_deve_retornar_html_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get("/exercise-1")
+
+    assert response.status_code == HTTPStatus.OK
+    assert "<h1>Olá mundo!</h1>" in response.text
