61e68cb681cb1cd568e7d8fdc710e7234b77c827
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2024-05-07 00:57:56 -0300

    Implementação de detalhe do user

--- tests/test_app.py
+++ tests/test_app.py
@@ -47,6 +47,24 @@ def test_read_users(client):
     }
 
 
+def test_detail_user(client):
+    response = client.get("/users/1/")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "username": "alice",
+        "email": "alice@example.com",
+        "id": 1,
+    }
+
+
+def test_detail_user_not_found(client):
+    response = client.get("/users/0/")
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
+
+
 def test_update_user(client):
     response = client.put(
         "/users/1/",
