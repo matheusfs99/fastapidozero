d21cb3d94f543e4e0d247d92b195ad38ecefdbb8
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2024-05-09 23:32:18 -0300

    Atualizando endpoints para usar o banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get("/")
@@ -33,29 +35,38 @@ def test_create_user(client):
     }
 
 
+def test_create_user_already_registered(client, user):
+    response = client.post(
+        "/users/",
+        json={
+            "username": user.username,
+            "email": user.email,
+            "password": "secret"
+        }
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {"detail": "Username already registered"}
+
+
 def test_read_users(client):
     response = client.get("/users/")
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        "users": [
-            {
-                "username": "alice",
-                "email": "alice@example.com",
-                "id": 1,
-            }
-        ]
-    }
+    assert response.json() == {"users": []}
 
 
-def test_detail_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get("/users/")
+    assert response.json() == {"users": [user_schema]}
+
+
+def test_detail_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
     response = client.get("/users/1/")
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        "username": "alice",
-        "email": "alice@example.com",
-        "id": 1,
-    }
+    assert response.json() == user_schema
 
 
 def test_detail_user_not_found(client):
@@ -65,7 +76,7 @@ def test_detail_user_not_found(client):
     assert response.json() == {"detail": "User not found"}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         "/users/1/",
         json={
@@ -94,12 +105,10 @@ def test_update_user_not_found(client):
     )
 
     assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {
-        "detail": "User not found"
-    }
+    assert response.json() == {"detail": "User not found"}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete("/users/1")
 
     assert response.status_code == HTTPStatus.OK
--- tests/test_db.py
+++ tests/test_db.py
@@ -4,7 +4,9 @@ from fast_zero.models import User
 
 
 def test_create_user(session):
-    new_user = User(username="matheus", email="matheus@mail.com", password="senha123")
+    new_user = User(
+        username="matheus", email="matheus@mail.com", password="senha123"
+    )
     session.add(new_user)
     session.commit()
 
